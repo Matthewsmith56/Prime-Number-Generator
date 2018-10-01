d0250e10a69cea17792aa991afda2a05e771d729
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-10-01 10:03:21 -0400

    Final Program

--- Prime Number Generator/Program.cs
+++ Prime Number Generator/Program.cs
@@ -10,6 +10,35 @@ namespace Prime_Number_Generator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number you would like to find primes up to then press enter");
+
+            int max = (Convert.ToInt32(Console.ReadLine()) - 1);
+            int j = 0;
+            var nums = Enumerable.Range(2, max).ToList();
+
+            for (int i = 2; i <= max;)
+            {
+                if (j == max)
+                {
+                    i++;
+                    j = 0;
+                }
+                else if (j < max)
+                {
+                    if (nums[j] % i == 0 && nums[j] != i)
+                    {
+                        nums[j] = 0;
+                    }
+                    j++;
+                }
+            }
+            foreach (var k in nums)
+            {
+                if (k != 0)
+                {
+                    Console.WriteLine(k);
+                }
+            }
         }
     }
 }
