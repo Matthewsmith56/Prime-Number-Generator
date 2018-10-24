03c6982b29dd96b5c05e2ff4cb1ec23081d6f0dd
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-10-24 13:16:20 -0400

    formated text

--- Prime Number Generator/Program.cs
+++ Prime Number Generator/Program.cs
@@ -11,10 +11,10 @@ namespace Prime_Number_Generator
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the prime number generator. How would you like to generate primes? " +
-                              "\n1 - Generate and list primes within a range of numbers" +
-                              "\n2 - Generate primes as X and composite numbers as 0 to visually display distribution within a range of numbers" +
-                              "\n3 - Print to desktop Markdown file");
-
+                              "\n\n1 - Generate and list primes within a range of numbers" +
+                              "\n\n2 - Generate primes as X and composite numbers as 0 to visually display distribution within a range of numbers" +
+                              "\n\n3 - Print to desktop Markdown file");
+                                
             string selection = Console.ReadLine();
 
             Console.WriteLine("Enter the number you would like to find primes up to");
